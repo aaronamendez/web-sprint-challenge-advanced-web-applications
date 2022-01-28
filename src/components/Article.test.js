1e7ea7d734d929dc0baf0c8ec3c57066af0fa2b8
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-28 07:51:59 -0500

    Finished two tests, moving onto the other two.

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,18 +5,30 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+// import { render } from 'react-dom';
+import { render, screen } from '@testing-library/react';
 
-test('renders component without errors', ()=> {
+test('renders component without errors', () => {
+	render(<Article article={[]} handleDelete={[]} handleEditSelect={[]} />);
 });
 
-test('renders headline, author from the article when passed in through props', ()=> {
+test('renders headline, author from the article when passed in through props', () => {
+	render(<Article article={[]} handleDelete={[]} handleEditSelect={[]} />);
+
+	const headline = screen.getByTestId(/headline/i);
+	const author = screen.getByTestId(/author/i);
+
+	expect(headline).toBeInTheDocument();
+	expect(author).toBeInTheDocument();
 });
 
-test('renders "Associated Press" when no author is given', ()=> {
+test('renders "Associated Press" when no author is given', () => {
+	render(<Article article={[]} handleDelete={[]} handleEditSelect={[]} />);
+
+	expect();
 });
 
-test('executes handleDelete when the delete button is pressed', ()=> {
-});
+test('executes handleDelete when the delete button is pressed', () => {});
 
-//Task List: 
-//1. Complete all above tests. Create test article data when needed.
+//Task List:
+//1. Complete all above tests. Create test article data when
