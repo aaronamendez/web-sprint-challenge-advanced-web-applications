ae248042824bf5be29b8d3876d181ddaf58b91e5
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-28 06:53:25 -0500

    Finished the logout component tasks.

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
-const Logout = () => {        
-    return(<div></div>);
-}
+const Logout = () => {
+	const { push } = useHistory();
+	useEffect(() => {
+		axiosWithAuth()
+			.post('/logout')
+			.then((res) => {
+				localStorage.removeItem('token');
+				push('/');
+			})
+			.catch((err) => console.log(err));
+	}, []);
+	return <div></div>;
+};
 
 export default Logout;
 
